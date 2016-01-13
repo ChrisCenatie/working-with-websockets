db42a686d945377a7c9703c7f182deafe4f80e70
Chris Cenatiempo <etothe2pieye@gmail.com>
2016-01-13 09:47:22 -0700

    Added websockets for clients.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const express = require('express');
-
 const app = express();
+const socketIo = require('socket.io');
 
 app.use(express.static('public'));
 
@@ -12,8 +12,18 @@ var server = http.createServer(app)
                    console.log('Listening on port' + port + '.');
                  });
 
+const io = socketIo(server);
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+  });
 });
 
 module.exports = server;
